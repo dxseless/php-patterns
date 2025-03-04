a1219b39fa3bef7bb30f0d40871f7fd9b97873b0
dxseless <dm739165@gmail.com>
2025-03-04 23:49:10 +0300

    add singleton

---
+++ creational/singleton.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Singleton - порождающий паттерн проектирования
+ * 
+ * Класс может запретить создание дополнительных экземплеров, перехватывая
+ * запросы на создание новых объектов.
+ * И он же способен предоставить доступ  к своему экземпляру.
+ */
+class Singleton
+{
+    private static $instance;
+
+    /**
+     * Запрещаем создавать новые инстансы через new
+     */
+    private function __construct()
+    {}
+
+    /**
+     * Запрещаем клонировать в новые инстансы через clone
+    */
+    private function __clone()
+    {}
+
+    /**
+     * Клиенты осуществляют доступ к одиночке исключительно
+     * через метод getInstance (название может быть любым)
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+}
+
+$one = Singleton::getInstance();
+$second = Singleton::getInstance();
+
+// Проверка объекта на идентичность. Даст true, если это один и тот же объект
+var_dump($one === $second);
